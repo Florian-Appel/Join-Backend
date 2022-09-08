cd12e3c8069efcfbd32cbbfab6297ea1223baa8e
Daniel Stoehr <daniel.stoehr@boschrexroth.de>
2022-09-08 09:37:50 +0200

    add register link

--- join_backend/urls.py
+++ join_backend/urls.py
@@ -19,10 +19,11 @@ from rest_framework import routers
 from scrum import views
 
 router = routers.DefaultRouter()
-router.register(r'tasks', views.TaskViewSet)
+router.register(r"tasks", views.TaskViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
+    path("register", views.RegisterUserAPIView.as_view()),
 ]
--- scrum/views.py
+++ scrum/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import generics
 
-from scrum.serializers import TaskSerializer
+from scrum.serializers import RegisterSerializer, TaskSerializer, UserSerializer
 from .models import Task
 
 # Create your views here.
+
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     # permission_classes = [IsAuthenticated]
+
+
+class UserDetailAPI(APIView):
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (AllowAny,)
+
+    def get(self, request, *args, **kwargs):
+        user = User.objects.get(id=request.user.id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class RegisterUserAPIView(generics.CreateAPIView):
+    # permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
