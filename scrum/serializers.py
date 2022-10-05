8796c3855075b1251d97cdbdfdf5ad3313dc9294
Daniel Stöhr <stoehr-daniel@web.de>
2022-10-05 11:11:33 +0200

    reset nested view

--- scrum/serializers.py
+++ scrum/serializers.py
@@ -5,30 +5,29 @@ from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = "__all__"
+
 
 class PrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = Priority
-        fields = '__all__'
+        fields = "__all__"
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "first_name", "last_name", "username"]
+
+
 class TaskSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-    priority = PrioritySerializer()
-    user = UserSerializer()
     class Meta:
         model = Task
-        fields = '__all__'
-
-
-
+        fields = "__all__"
 
 
 class RegisterSerializer(serializers.ModelSerializer):
